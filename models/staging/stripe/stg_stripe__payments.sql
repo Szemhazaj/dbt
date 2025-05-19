2cb5a71aeff721c530bf4887b7d7bf2dc51f1369
Jacek Tkaczyk <tenten223@gmail.com>
2025-05-19 19:50:31 +0000

    complete models module

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,7 @@ select
     orderid as order_id,
     paymentmethod,
     status,
-    amount,
+    -- convert cents to dollars
+    amount/100 as amount,
     created
 from {{ source('stripe', 'payment') }}
